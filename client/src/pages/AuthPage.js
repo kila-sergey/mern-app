cb4f232f51c6ff6257134c77b8c86111beb285e8
kila-sergey <kila-sergey@yandex.ru>
2020-02-12 23:57:26 +0300

    commit

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.scss'
 
 const AuthPage = () => {
+  const [form,setForm] = useState({
+    email:'',
+    password:''
+  });
+
+  const changeHandler = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]:event.target.value
+    })
+  }
   return (
     <div className="row">
       <div className="col s6 offset-s3">
         <h1>Сократи Ссылку</h1>
-        <div className="card blue darken-1">
+        <div className="card">
           <div className="card-content white-text">
             <span className="card-title ">Авторизация</span>
           </div>
           <form autoComplete="off">
             <div className="input-field">
-              <input placeholder="Email" id="email" type="text" className="validate" />
+              <input 
+                placeholder="Email" 
+                id="email" type="text" 
+                className="validate" 
+                name="email"
+                onChange={changeHandler} />
               <label htmlFor="email">Email</label>
             </div>
             <div className="input-field">
-              <input placeholder="Password" id="password" type="text" className="validate" autocomplete="off" />
+              <input 
+                placeholder="Password" 
+                id="password" 
+                type="text" 
+                className="validate" 
+                autocomplete="off" 
+                name="password"
+                onChange={changeHandler}/>
               <label htmlFor="password">Password</label>
             </div>
           </form>
